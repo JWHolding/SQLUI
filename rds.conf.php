e0949b8909ba9b65eb612876035dc2864dd8dfb4
Jacob <jacob5holding@hotmail.com>
2018-01-31 17:39:13 +0000

    initial commit

---
+++ rds.conf.php
@@ -0,0 +1,1 @@
+<?php $RDS_URL="exampledb.ctemptlenqx0.eu-west-2.rds.amazonaws.com"; $RDS_DB="exampleDB"; $RDS_user="jwholding"; $RDS_pwd="Pa$$w0rd"; ?>
---
+++ sql/addressbook.sql
@@ -0,0 +1,2 @@
+CREATE TABLE address (id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(30), lastname VARCHAR(30), phone VARCHAR(30), email VARCHAR(30));
+INSERT INTO address (firstname, lastname, phone, email) VALUES ( "Roberto", "Johnson", "123-456-7890", "roberto@someaddress.com"), ( "Jane", "Doe", "010-110-1101", "janedoe@someotheraddress.org" );
